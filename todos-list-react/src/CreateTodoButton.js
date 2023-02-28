c6993fe1f24397ea8350132649a052442fc43e79
Memento96 <911sog@gmail.com>
2023-02-28 00:27:03 -0500

    Added functions
    tested the creation of functions onClick and onChange. Used alerts and console logs but will later try to create pop-ups

--- todos-list-react/src/CreateTodoButton.js
+++ todos-list-react/src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import './CreateTodoButton.css'
 
 function CreateTodoButton (){
+
+    const onClickButton = () => {
+        alert('Here we should have a modal');
+    };
+
     return(
-        <button className="CreateTodoButton">+</button>
+        <button className="CreateTodoButton"
+        onClick={onClickButton}
+        >
+            +
+        </button>
     );
 }
 
--- todos-list-react/src/TodoItem.js
+++ todos-list-react/src/TodoItem.js
@@ -2,17 +2,31 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
+
+  const onComplete = () => {
+    alert('The task ' + props.text + ' has been successfully completed');
+  }
+
+  const onDelete = () => {
+    alert('The task ' + props.text + ' has been removed');
+  }
+
   return (
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">X</span>
+      <span className="Icon Icon-delete"
+       onClick={onDelete}
+      >
+        X
+      </span>
     </li>
   );
 }
